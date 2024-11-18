bb3dba9564a3389628d1a8fdd4802b6dd0825718
hcarminati <hscarminati@hotmail.com>
2024-11-18 15:21:30 -0500

    welcome message

--- cogs/send.py
+++ cogs/send.py
@@ -1,46 +1,55 @@
 import discord
 from discord.ext import commands
 
+
 class Send(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print(f'send cog')
+        print(f'Send cog loaded')
 
-    @commands.command(name="send", help="blah")
-    async def send(self, ctx, channel_name: str, message: str):
+    @commands.command(name="send", help="Sends a welcome message to the specified channel")
+    async def send(self, ctx, channel_name: str):
+        print("Command triggered")
+
+        # Get the channel by name
         channel = discord.utils.get(ctx.guild.text_channels, name=channel_name)
+
+        # The message to send
         welcome_message = """
-                Welcome to SkillShare! 
-                
-                - Join a new Skill
-                1. Go to #join-new-skill 
-                2. Use the **$join <skill>** command 
+***Welcome to SkillShare!***
 
-                - Browse Existing Skills 
-                1. Go to #join-new-skill 
-                2. Use the **$available** command 
+**✦ Browse Existing Skills**
+1. Go to #join-new-skill
+2. Use the **$available** command
 
-                - Suggest a new Skill 
-                1. Go to #suggest-new-skill 
-                2. Use the **$suggest <skill>** command
+**✦ Join a new Skill**
+1. Go to #join-new-skill
+2. Use the **$available** command to browse existing skills
+3. Use the **$join <skill>** command.
+        * Replace `<skill>` with the skill you want to join.
 
-                - Asking a Question
-                1. Make sure you are in a skill channel
-                2. Use the **$ask <question>** command
+**✦ Suggest a new Skill**
+1. Go to #suggest-new-skill
+2. Use the **$suggest <skill>** command
+        * Replace `<skill>` with the skill you want to suggest.
 
-                - Request a Challenge 
-                1. Make sure you are in a skill channel
-                2. Use the **$new** command
-                """
-            # Send the welcome message to the current channel
-        await channel.send(welcome_message)
+**✦ Asking a Question**
+1. Make sure you are in a skill channel
+2. Use the **$ask <question>** command
+        * Replace `<question>` with the question you want to ask
 
+**✦ Request a Challenge**
+1. Make sure you are in a skill channel
+2. Use the **$new** command
+        """
+
+        # Send the message to the selected channel
         if channel:
-            await channel.send(message)  # Send the message to the channel
-            await ctx.send(f'Message sent to {channel.mention}')
+            await channel.send(welcome_message)
         else:
-            await ctx.send(f'Channel "{channel_name}" not found.')
+            await ctx.send("Channel not found!")
 
 
+# Set up the cog
 async def setup(bot):
     await bot.add_cog(Send(bot))
